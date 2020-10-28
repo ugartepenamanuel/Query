c9a2d3ec1d287f3f09d94357053213aa7b5a708b
ugartepenamanuel <72375041+ugartepenamanuel@users.noreply.github.com>
2020-10-28 18:12:02 +0100

    Update consultas.js

---
+++ src/consultas.js
@@ -0,0 +1,66 @@
+db . consulta . insertMany ([ 
+    {  
+        elemento :  "agenda" ,  
+        cantidad :  25 , 
+        tamaño :  {  h :  14 ,  w :  21 ,  uom :  "cm"  },  
+        estado :  "A" 
+     }, 
+
+    {  
+        elemento :  "cuaderno" ,  
+        cantidad :  50 ,  
+        tamaño :  {  h :  8.5 ,  w :  11 ,  uom : "en"  },  
+        estado :  "A"  
+    }, 
+
+    {  
+        elemento :  "papel" , 
+        cantidad :  100 ,  
+        tamaño :  {  h :  8.5 ,  w :  11 ,  uom :  "en"  },  
+        estado :  "D" 
+    }, 
+
+    {  
+        elemento :  "planificador" ,  
+        cantidad :  75 ,  
+        tamaño :  {  h :  22.85 ,  w :  30 ,  uom : "cm"  },  
+        estado :  "D" 
+     }, 
+
+    {  
+       elemento :  "postal" ,  
+       cantidad :  45 ,  
+       tamaño :  {  h :  10 ,  w :  15.25 ,  uom :  "cm"  },  
+       estado :  "A" 
+
+    },
+
+
+    {  
+        elemento :  "papel" ,  
+        cantidad :  55 ,  
+        tamaño :  {  h :  12 ,  w :  15.20 ,  uom :  "cm"  },  
+        estado :  "B" 
+ 
+     },
+
+
+     {  
+        elemento :  "goma" ,  
+        cantidad :  60 ,  
+        tamaño :  {  h :  11 ,  w :  15.26 ,  uom :  "cm"  },  
+        estado :  "A" 
+ 
+     }, 
+
+
+     {  
+        elemento :  "retulador" ,  
+        cantidad :  60 ,  
+        tamaño :  {  h :  11 ,  w :  15.26 ,  uom :  "cm"  },  
+        estado :  "A" 
+ 
+     }
+
+
+ ]);
